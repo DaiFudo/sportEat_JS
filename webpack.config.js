42c588b528993ba0cd0bf5948e1181b90d54f9a0
Fudo <dai.fudo@mail.ru>
2020-07-23 19:45:40 +0300

    two commut

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
-  
 'use strict';
 
 let path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/js/script.js',
+  mode: 'production',
+  entry: './js/script.js',
   output: {
     filename: 'bundle.js',
     path: __dirname + '/js'
@@ -14,5 +13,22 @@ module.exports = {
 
   devtool: "source-map",
 
-  module: {}
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', {
+                debug: true,
+                corejs: 3,
+                useBuiltIns: "usage"
+            }]]
+          }
+        }
+      }
+    ]
+  }
 };
